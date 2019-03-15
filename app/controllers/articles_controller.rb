52513cdd465394fc5e106d950fd7faf99926ef4b
hiroki-okazaki <1031okazaki@gmail.com>
2019-03-15 23:37:57 +0900

    routes追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,7 +35,7 @@ class ArticlesController < ApplicationController
     def set_article
       @article = Article.find(params[:id])
     end
-
+    
     def article_params
       params.require(:article).permit(:title, :body)
     end
